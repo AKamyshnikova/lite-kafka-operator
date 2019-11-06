31e41a1f06814a8ac1ca747f21ea7a157e4f7a09
Svimba <psvimbersky@mirantis.com>
2019-11-06 15:11:29 +0100

    Initial commit

---
+++ doc.md
@@ -0,0 +1,1 @@
+$ operator-sdk add api --api-version litekafka.operator.svimba.cz/v1alpha1 --kind KafkaCluster
---
+++ pkg/apis/litekafka/v1alpha1/doc.go
@@ -0,0 +1,4 @@
+// Package v1alpha1 contains API Schema definitions for the litekafka v1alpha1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=litekafka.operator.svimba.cz
+package v1alpha1
