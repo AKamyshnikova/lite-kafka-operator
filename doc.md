a3b50aa02ad280322eb732883c1a3c8c653682a1
Svimba <psvimbersky@mirantis.com>
2019-11-28 15:22:22 +0100

    Change API group

--- doc.md
+++ doc.md
@@ -2,7 +2,7 @@
 # Created
 
 ### API
-$ operator-sdk add api --api-version litekafka.operator.svimba.cz/v1alpha1 --kind KafkaCluster
+$ operator-sdk add api --api-version litekafka.operator.mirantis.com/v1alpha1 --kind KafkaCluster
 
 ### Controller
-$ operator-sdk add controller --api-version litekafka.operator.svimba.cz/v1alpha1 --kind all
+$ operator-sdk add controller --api-version litekafka.operator.mirantis.com/v1alpha1 --kind all
--- pkg/apis/litekafka/v1alpha1/register.go
+++ pkg/apis/litekafka/v1alpha1/register.go
@@ -2,7 +2,7 @@
 
 // Package v1alpha1 contains API Schema definitions for the litekafka v1alpha1 API group
 // +k8s:deepcopy-gen=package,register
-// +groupName=litekafka.operator.svimba.cz
+// +groupName=litekafka.operator.mirantis.com
 package v1alpha1
 
 import (
@@ -12,7 +12,7 @@ import (
 
 var (
 	// SchemeGroupVersion is group version used to register these objects
-	SchemeGroupVersion = schema.GroupVersion{Group: "litekafka.operator.svimba.cz", Version: "v1alpha1"}
+	SchemeGroupVersion = schema.GroupVersion{Group: "litekafka.operator.mirantis.com", Version: "v1alpha1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: SchemeGroupVersion}
